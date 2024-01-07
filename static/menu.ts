04c573b7bef5a5f4ecd17920f39f9ee412b99c76
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2024-01-07 13:49:01 -0300

    ✨ feat(menu.ts): add menuItems array to store menu items for the application's navigation menu

---
+++ static/menu.ts
@@ -0,0 +1,20 @@
+export const menuItems = [
+  {
+    id: 1,
+    title: 'Home',
+    icon: 'home',
+    link: '/home',
+  },
+  {
+    id: 2,
+    title: 'About',
+    icon: 'info',
+    link: '/about',
+  },
+  {
+    id: 3,
+    title: 'Contact',
+    icon: 'phone',
+    link: '/contact',
+  },
+]
