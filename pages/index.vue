5fefca2c89a9498b5274c9ece7d20a521995d0d4
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2024-01-07 13:49:21 -0300

    ✨ feat(index.vue): add a header component with a logo and navigation menu
    The `index.vue` file was added with a template for a header component. The header includes a logo and a navigation menu. The logo is displayed as an image, and the navigation menu is rendered as a list of links. Each link corresponds to a menu item with a title and a link.

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="max-w-[1366px] mx-auto">
+    <header class="flex w-full justify-between">
+      <nuxt-link
+        to="/"
+        class="flex items-center w-32 h-32 rounded-full overflow-hidden"
+        title="Home"
+      >
+        <img
+          src="https://media.discordapp.net/attachments/899855418065567746/1162464082096566283/image.png?ex=65aac60a&is=6598510a&hm=de64d8005bb0a81eed5b604e888ac9d4c0c16f4461f82352e7bc46f76defa2fd&=&format=webp&quality=lossless"
+          alt="Logo"
+        >
+      </nuxt-link>
+      <nav>
+        <ul class="flex w-full items-center h-32">
+          <li
+            v-for="menuItem in menuItems"
+            :key="menuItem.id"
+            class="p-4 hover:border-b-4 hover:border-green-500 hover:text-green-500 transition-all ease-in-out duration-300"
+          >
+            <nuxt-link
+              :to="menuItem.link"
+            >
+              {{ menuItem.title }}
+            </nuxt-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  </div>
+</template>
