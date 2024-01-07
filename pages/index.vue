1dec1d8fda145df864e1b4de79ddd438a64034ee
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2024-01-07 19:06:12 -0300

    🔧 fix(index.vue): rearrange class order in ul element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in li element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability and maintain consistency 🔧 fix(index.vue): rearrange class order in a element to improve readability

--- pages/index.vue
+++ pages/index.vue
@@ -13,12 +13,12 @@
         >
       </nuxt-link>
       <nav class="relative">
-        <ul class="flex w-full items-center">
+        <ul class="flex items-center w-full">
           <li
             v-for="menuItem in menuItems"
             :key="menuItem.id"
             :title="`Ir para ${menuItem.title}`"
-            class="p-4 border-b-4 border-transparent hover:border-orange-500 hover:text-orange-500 transition-all ease-in-out duration-300"
+            class="p-4 transition-all duration-300 ease-in-out border-b-4 border-transparent hover:border-orange-500 hover:text-orange-500"
           >
             <nuxt-link
               :to="menuItem.link"
@@ -43,7 +43,7 @@
               to="https://github.com/ricardo564?tab=projects"
               target="_blank"
               rel="noopener noreferrer"
-              class="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded"
+              class="px-4 py-2 font-bold text-white bg-orange-500 rounded hover:bg-orange-600"
               title="See my projects on GitHub"
             >
               Other Projects
@@ -51,37 +51,37 @@
             <nuxt-link
               href="#section-2"
               rel="noopener noreferrer"
-              class="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded ml-4"
+              class="px-4 py-2 ml-4 font-bold text-white bg-orange-500 rounded hover:bg-orange-600"
             >
               Show more
             </nuxt-link>
           </div>
         </div>
-        <div class="flex min-h-screen h-full w-full items-center justify-center">
+        <div class="flex items-center justify-center w-full h-full min-h-screen">
           <div class="max-w-[30rem] w-full h-full max-h-[30rem] rounded-full overflow-hidden border-8 border-orange-600">
             <img
               src="https://cdn.discordapp.com/attachments/899855418065567746/1193614252859215983/image.png?ex=65ad5ae6&is=659ae5e6&hm=b160fdba75485f3bf045a42b13748adb35ab089e302851ec174a5143cf985285&"
-              class="w-42 h-42 object-cover mx-auto my-auto object-bottom"
+              class="object-cover object-bottom mx-auto my-auto w-42 h-42"
               alt="Logo"
             >
           </div>
         </div>
       </section>
       <section
-        class="grid min-h-screen max-h-[600px] bg-white px-1 bg-[url('https://cdn.discordapp.com/attachments/1162465536077205535/1163838239749193818/image.png?ex=65a68b53&is=65941653&hm=9bf5c783080fe32bf13c6941efcf3e6f40436001d6537f78dfc23e837046cede&')] bg-fixed bg-contain bg-no-repeat z-[10]"
+        class="flex flex-col min-h-screen max-h-[600px] bg-white gap-16 px-1 bg-[url('https://cdn.discordapp.com/attachments/1162465536077205535/1163838239749193818/image.png?ex=65a68b53&is=65941653&hm=9bf5c783080fe32bf13c6941efcf3e6f40436001d6537f78dfc23e837046cede&')] bg-fixed bg-contain bg-no-repeat z-[10]"
         id="section-2"
       >
-        <h2 class="text-4xl font-bold text-left mx-auto pt-16">
+        <h2 class="pt-16 mx-auto text-4xl font-bold text-left">
           My Skills
         </h2>
-        <div class="flex w-full flex-wrap gap-4">
+        <div class="flex flex-wrap w-full gap-4">
           <div
             v-for="skill in skills"
             :key="skill.id"
             class="flex h-44 w-[17rem] relative border py-5"
           >
             <div class="flex flex-col h-full">
-              <div class="flex flex-col gap-4 absolute bottom-2 p-2">
+              <div class="absolute flex flex-col gap-4 p-2 bottom-2">
                 <h3 class="text-2xl font-bold text-center">
                   {{ skill.name }}
                 </h3>
@@ -91,7 +91,7 @@
               </div>
               <img
                 :src="skill.image"
-                class="absolute w-24 ml-auto right-2 top-2 h-24 object-cover mx-auto my-auto object-bottom"
+                class="absolute object-cover object-bottom w-24 h-24 mx-auto my-auto ml-auto right-2 top-2"
                 alt="Logo"
               >
             </div>
