8de96285960576b1eb3dd94f5e60755a89f85c4b
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2024-01-07 15:21:23 -0300

    📝 chore(skills.ts): add skills data to static/skills.ts file
    The static/skills.ts file was created and populated with an array of skill objects. Each skill object contains an id, name, level, and image URL. This file will be used to display skills information on the website.

---
+++ static/skills.ts
@@ -0,0 +1,50 @@
+export const skills = [
+  {
+    id: 's1',
+    name: 'HTML semantico',
+    level: '90%',
+    image: 'https://cdn.discordapp.com/attachments/899855418065567746/1193620665153101966/html-5_234_1.png?ex=65ad60de&is=659aebde&hm=55c882815507f1ccb041ebdeaa98dfd1abb249a04c2af9ee7523a0ab899d13bb&',
+  },
+  {
+    id: 's2',
+    name: 'CSS',
+    level: '80%',
+    image: 'https://cdn.discordapp.com/attachments/899855418065567746/1193619134253105192/css-3-logo-AF06D75231-seeklogo.com_1.png?ex=65ad5f71&is=659aea71&hm=719306e1dc4ae06343ccd3bd3dbe93abe23122fc05b561c5370852403c1aa585&',
+  },
+  {
+    id: 's3',
+    name: 'Javascript',
+    level: '80%',
+    image: 'https://cdn.discordapp.com/attachments/899855418065567746/1193614252859215983/image.png?ex=65ad5ae6&is=659ae5e6&hm=b160fdba75485f3bf045a42b13748adb35ab089e302851ec174a5143cf985285&',
+  },
+  {
+    id: 's4',
+    name: 'Typescript',
+    level: '70%',
+    image: 'https://cdn.discordapp.com/attachments/899855418065567746/1193614252859215983/image.png?ex=65ad5ae6&is=659ae5e6&hm=b160fdba75485f3bf045a42b13748adb35ab089e302851ec174a5143cf985285&',
+  },
+  {
+    id: 's5',
+    name: 'Vue 3',
+    level: '70%',
+    image: 'https://cdn.discordapp.com/attachments/899855418065567746/1193614252859215983/image.png?ex=65ad5ae6&is=659ae5e6&hm=b160fdba75485f3bf045a42b13748adb35ab089e302851ec174a5143cf985285&',
+  },
+  {
+    id: 's6',
+    name: 'Nuxt 3',
+    level: '60%',
+    image: 'https://cdn.discordapp.com/attachments/899855418065567746/1193614252859215983/image.png?ex=65ad5ae6&is=659ae5e6&hm=b160fdba75485f3bf045a42b13748adb35ab089e302851ec174a5143cf985285&',
+  },
+  {
+    id: 's7',
+    name: 'Tailwindcss',
+    level: '70%',
+    image: 'https://cdn.discordapp.com/attachments/899855418065567746/1193614252859215983/image.png?ex=65ad5ae6&is=659ae5e6&hm=b160fdba75485f3bf045a42b13748adb35ab089e302851ec174a5143cf985285&',
+  },
+  {
+    id: 's8',
+    name: 'Astrojs',
+    level: '50%',
+    image: 'https://cdn.discordapp.com/attachments/899855418065567746/1193614252859215983/image.png?ex=65ad5ae6&is=659ae5e6&hm=b160fdba75485f3bf045a42b13748adb35ab089e302851ec174a5143cf985285&',
+  },
+]
